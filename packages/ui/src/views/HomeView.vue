3cd6c5fac921b06bead8bbca444bfe3d64de4e5a
Dmitrii <lexur-02@yandex.ru>
2023-12-16 00:13:14 +0500

    clear code a little bit

--- packages/ui/src/views/HomeView.vue
+++ packages/ui/src/views/HomeView.vue
@@ -1,12 +1,4 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
   </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({})
-export default class HomeView extends Vue {}
-</script>
