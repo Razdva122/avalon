ba50e5ecffe8afedb64d0da0c4bc35a77d194cc2
Dmitrii <lexur-02@yandex.ru>
2023-12-15 22:34:24 +0500

    board for game

--- packages/ui/src/main.ts
+++ packages/ui/src/main.ts
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store'
 
-createApp(App).use(store).use(router).mount('#app')
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+createApp(App).use(store).use(router).use(vuetify).mount('#app')
---
+++ packages/ui/src/views/GameView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="game">
+    <h1>This is game page</h1>
+		<div class="board-container">
+			<img class="game-board" alt="board" src="../assets/board.jpeg">
+			<div class="player-container" 
+				v-for="n in 10" 
+				:style="{ 'transform': 'rotate(' + (360 / 10 * (n - 1) + 180) + 'deg)' }"
+			>
+				<img
+					class="player-frame" 
+					alt="frame" 
+					src="../assets/player-frame.png"
+					:style="{ 'transform': 'translate(-50%, -50%) rotate(-' + (360 / 10 * (n - 1) + 180) + 'deg)' }"
+				>
+			</div>
+		</div>
+  </div>
+</template>
+
+<style lang="scss">
+.board-container {
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: relative;
+	width: 100vw;
+}
+
+.game-board {
+	width: 800px;
+}
+
+.player-container {
+	position: absolute;
+	top: 0;
+	width: 600px;
+	height: 600px;
+}
+
+.player-frame {
+	position: absolute;
+	width: 115px;
+}
+</style>
