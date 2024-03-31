6aa49bed6000a17b4d40a42730cb1e640782ff82
Dmitrii <lexur-02@yandex.ru>
2024-03-31 14:25:33 +0500

    disable webvisor

--- packages/ui/const.js
+++ packages/ui/const.js
@@ -1,34 +1,18 @@
 const yaMetrika = `<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-	(function (m, e, t, r, i, k, a) {
-		m[i] =
-			m[i] ||
-			function () {
-				(m[i].a = m[i].a || []).push(arguments);
-			};
-		m[i].l = 1 * new Date();
-		for (var j = 0; j < document.scripts.length; j++) {
-			if (document.scripts[j].src === r) {
-				return;
-			}
-		}
-		(k = e.createElement(t)),
-			(a = e.getElementsByTagName(t)[0]),
-			(k.async = 1),
-			(k.src = r),
-			a.parentNode.insertBefore(k, a);
-	})(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+<script type="text/javascript" >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-	ym(96679204, 'init', {
-		clickmap: true,
-		trackLinks: true,
-		accurateTrackBounce: true,
-		webvisor: true,
-	});
+   ym(96679204, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+   });
 </script>
-<noscript
-	><div><img src="https://mc.yandex.ru/watch/96679204" style="position: absolute; left: -9999px" alt="" /></div
-></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/96679204" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->`;
 
 const gtag = `<!-- Google tag (gtag.js) -->
