77eacbcae12e0d4cee453bca5170fbc269c321a6
Dmitrii <lexur-02@yandex.ru>
2024-10-16 13:19:20 +0500

    updated translates

---
+++ packages/ui/src/helpers/i18n/translates/langs/pages/seo/wiki.js
@@ -0,0 +1,24 @@
+module.exports.wiki = {
+  en: {
+    title: 'Avalon: The Resistance | Wiki',
+    description:
+      "Your go-to 'Avalon: The Resistance' Wiki resource. Find detailed guides on playing Avalon, rules explanation, and strategy tips.",
+    keywords: ['Wiki', 'Rules', 'How to play'],
+  },
+  ru: {
+    title: 'Авалон: Сопротивление | Вики',
+    description:
+      "Ваш надежный ресурс по игре 'Авалон: Сопротивление'. Найдите подробные руководства по игре в Авалон, объяснения правил и советы по стратегии.",
+    keywords: ['Вики', 'Правила', 'Как играть'],
+  },
+  zh_TW: {
+    title: '阿瓦隆：反抗勢力 | 維基',
+    description: '您的首選「阿瓦隆：反抗勢力」維基資源。尋找有關玩阿瓦隆的詳細指南、規則解釋以及策略提示。',
+    keywords: ['維基', '規則', '玩法'],
+  },
+  zh_CN: {
+    title: '阿瓦隆：反抗组织 | 维基',
+    description: '您的一站式「阿瓦隆：反抗组织」维基资源。查找有关玩阿瓦隆的详细指南、规则解释和策略提示。',
+    keywords: ['维基', '规则', '怎么玩'],
+  },
+};
