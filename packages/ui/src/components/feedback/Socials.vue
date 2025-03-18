d0caac29003f1ba6ca557b91d2716d566f747d00
Maxim <mdzhivanyan@ozon.ru>
2025-03-18 16:52:06 +0300

    add main btn

--- packages/ui/src/components/feedback/Socials.vue
+++ packages/ui/src/components/feedback/Socials.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="d-flex">
-    <v-btn density="comfortable" variant="plain" class="social-button mr-2" size="x-small">
+  <div>
+    <v-btn density="comfortable" variant="plain" class="mr-2" icon>
       <a href="https://discord.gg/DR9cEDDNdN" target="_blank" class="social-icon discord-icon"></a>
     </v-btn>
-    <v-btn density="comfortable" variant="plain" class="social-button" size="x-small">
+    <v-btn density="comfortable" variant="plain" icon>
       <a href="https://github.com/Razdva122/avalon" target="_blank" class="social-icon github-icon"></a>
     </v-btn>
   </div>
@@ -16,27 +16,22 @@ export default defineComponent({});
 </script>
 
 <style scoped lang="scss">
-.v-btn.social-button {
-  padding: 0px;
-  width: 32px;
-  height: 32px;
-}
-
 .social-icon {
   width: 36px;
   height: 32px;
-  background-position: center;
-  background-size: contain;
+  mask-position: center;
+  mask-size: contain;
+  mask-repeat: no-repeat;
 }
 
 .discord-icon {
-  background-image: getIconPathByName('discord.png');
-  filter: invert(var(--v-icon-invert));
+  mask-image: getIconPathByName('discord.png');
+  background-color: rgb(var(--v-theme-text-primary));
 }
 
 .github-icon {
-  background-size: 115%;
-  background-image: getIconPathByName('github.png');
-  filter: invert(var(--v-icon-invert));
+  mask-size: 115%;
+  mask-image: getIconPathByName('github.png');
+  background-color: rgb(var(--v-theme-text-primary));
 }
 </style>
