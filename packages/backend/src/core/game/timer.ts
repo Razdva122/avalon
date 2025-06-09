c676d230cf378c6eeceb56143ffa8d389bb2d1cc
RazDva122 <lexur-02@yandex.ru>
2025-06-09 20:58:14 +0500

    remove log

--- packages/backend/src/core/game/timer.ts
+++ packages/backend/src/core/game/timer.ts
@@ -66,7 +66,6 @@ export class GameTimer {
     this.clearTimer();
 
     if (!this.isStageTimerEnabled(stage)) {
-      console.log(`[GameTimer] Timer not enabled for stage: ${stage}`);
       return;
     }
 
